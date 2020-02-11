826a53d13d375152aad33e0d2dc255e5728daac2
Cara Diann <cara.di42@gmail.com>
2020-02-10 19:27:50 -0800

    somewhat stable

---
+++ Assets/DOTS Testing Samples/Spawing At Target Positions/SpawnTargetDataAuthoring.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class SpawnTargetDataAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        var parent = dstManager.GetComponentData<Parent>(entity);
+
+        var ltwBuffer = conversionSystem.GetComponentDataFromEntity<LocalToWorld>();
+
+        dstManager.AddComponentData(entity, new SpawnTargetData());
+        
+        DynamicBuffer<SpawnTargetElementData> buffer;
+        if (dstManager.HasComponent<SpawnTargetElementData>(parent.Value) 
+            && dstManager.HasComponent<SpawnerData>(parent.Value))
+            buffer = dstManager.GetBuffer<SpawnTargetElementData>(parent.Value);
+        else
+            buffer = dstManager.AddBuffer<SpawnTargetElementData>(parent.Value);
+
+        buffer.Add(new SpawnTargetElementData() {Value = entity});
+    }
+}
+
+public struct SpawnTargetData : IComponentData
+{
+    public SpawnTargetData(int i)
+    {
+        
+    }
+}
---
+++ Assets/DOTS Testing Samples/Spawing At Target Positions/SpawnerSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+[AlwaysSynchronizeSystem]
+public class SpawnerSystem : JobComponentSystem
+{
+    private EntityCommandBufferSystem _entityCommandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        _entityCommandBufferSystem =
+            World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EntityCommandBufferSystem>();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var commandBuffer = _entityCommandBufferSystem.CreateCommandBuffer();
+
+        var childBuffer = _entityCommandBufferSystem.GetBufferFromEntity<Child>();
+        
+        Entities.WithoutBurst().ForEach((int entityInQueryIndex, in Entity e, in SpawnerData spawnerdata) =>
+        {
+            
+        }).Run();
+        
+        return default;
+    }
+}
